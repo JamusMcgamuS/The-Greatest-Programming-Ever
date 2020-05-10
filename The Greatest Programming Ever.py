0b5f03af9c5ec3326683d38f45a94774134eb56f
JamusMcgamuS <62674850+JamusMcgamuS@users.noreply.github.com>
2020-05-10 13:24:26 +1200

    Add files via upload

---
+++ The Greatest Programming Ever.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 21 20:45:10 2019
+
+@author: GGPC
+"""
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+print([aa for aa in a if aa < 5])      # [output] for [item] in [list] if [filter]
